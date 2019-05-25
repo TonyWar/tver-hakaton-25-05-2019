4a0b8a7f3d3525a3edf141cc731f8c7ccef031e7
Dima <purisdima@mail.ru>
2019-05-26 02:04:44 +0300

    update helper profile

--- src/app/components/helper-card/helper-card.component.ts
+++ src/app/components/helper-card/helper-card.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { UserProfile } from 'src/app/types/user.model';
+import { UserProfileService } from 'src/app/services/user-profile/user-profile.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-helper-card',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./helper-card.component.less']
 })
 export class HelperCardComponent implements OnInit {
+  @Input() helperId: string;
+  helper: UserProfile;
 
-  constructor() { }
+  constructor(
+    private userService: UserProfileService,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    this.userService.getUserById(this.helperId)
+    .subscribe((helper: UserProfile) => {
+      this.helper = helper;
+      console.log('helper', helper)
+    });
+  }
+
+  openProfile() {
+    this.router.navigate([`helper/profile/${this.helper.id}`])
   }
 
 }
