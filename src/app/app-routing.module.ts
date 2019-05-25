295da83eab5e37177303812ae5763516f505aff0
Tony Warlamof <tonywarlamoff@gmail.com>
2019-05-25 21:19:51 +0300

    Auto stash before merge of "develop" and "origin/develop"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,15 +6,17 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
 import { OlderComponent } from './profile/older/older.component';
 import { AdminComponent } from './profile/admin/admin.component';
 import { HelperComponent } from './profile/helper/helper.component';
+import { AuthGuard } from './guards/auth/auth.guard';
+import { OnlyAdminGuard } from './guards/only-admin/only-admin.guard';
 
 const routes: Routes = [
   { path: 'sign-in', component: SignInComponent },
-  { path: 'admin/add_older', component: OlderRegistryComponent },
-  { path: 'admin/add_task', component: AddTaskComponent },
-  { path: 'older/profile', component: OlderComponent },
-  { path: 'older/profile/:olderId', component: OlderComponent },
-  { path: 'admin/profile', component: AdminComponent },
-  { path: 'helper/profile', component: HelperComponent }
+  { path: 'admin/add_older', component: OlderRegistryComponent, canActivate: [AuthGuard] },
+  { path: 'admin/add_task', component: AddTaskComponent, canActivate: [AuthGuard] },
+  { path: 'older/profile', component: OlderComponent, canActivate: [AuthGuard] },
+  { path: 'older/profile/:olderId', component: OlderComponent, canActivate: [AuthGuard] },
+  { path: 'admin/profile', component: AdminComponent, canActivate: [AuthGuard, OnlyAdminGuard] },
+  { path: 'helper/profile', component: HelperComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
