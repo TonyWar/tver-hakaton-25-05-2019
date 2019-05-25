f20782f04b51b5db588a3f19f715ba9b564ac303
Tony Warlamof <tonywarlamoff@gmail.com>
2019-05-25 15:38:55 +0300

    add reactive form

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators, FormGroupDirective, NgForm } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-sign-in',
@@ -6,8 +16,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.less']
 })
 export class SignInComponent implements OnInit {
+  loginForm = this.fb.group({
+    phone: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
+  });
 
-  constructor() { }
+
+  matcher = new MyErrorStateMatcher();
+
+  constructor(
+    private readonly fb: FormBuilder,
+  ) { }
 
   ngOnInit() {
   }
