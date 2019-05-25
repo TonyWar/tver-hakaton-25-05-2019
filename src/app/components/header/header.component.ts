9cfad06cd31df2c2f77a073ed7f31c21d8a339f9
Tony Warlamof <tonywarlamoff@gmail.com>
2019-05-26 00:06:00 +0300

    add profile link

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { map } from 'rxjs/operators';
+import { UserRole } from 'src/app/types/user.model';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.less']
 })
 export class HeaderComponent implements OnInit {
+  profileLink = '';
 
-  constructor() { }
+  constructor(
+    private auth: AuthService
+  ) { }
 
   ngOnInit() {
+    this.auth.userAuthData$.pipe(map(user => {
+      if (!user) { return ''; }
+      let redirect = '';
+      switch (user.role) {
+        case UserRole.ADMIN:
+          redirect = 'admin/profile';
+          break;
+        case UserRole.HELPER:
+          redirect = 'helper/profile';
+          break;
+        case UserRole.OLDER:
+          redirect = 'older/profile';
+          break;
+      }
+      return redirect;
+    }))
+    .subscribe(res => {
+      this.profileLink = res;
+    })
   }
 
+  getProfileLink() {
+    return '';
+  }
 }
