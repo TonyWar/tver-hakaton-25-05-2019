bd6357467f1551bbce5c1b3fb8d1cab0fb1b7518
Tony Warlamof <tonywarlamoff@gmail.com>
2019-05-25 23:34:22 +0300

    fix not found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,11 @@ const routes: Routes = [
   { path: 'older/profile', component: OlderComponent, canActivate: [AuthGuard] },
   { path: 'older/profile/:olderId', component: OlderComponent, canActivate: [AuthGuard] },
   { path: 'admin/profile', component: AdminComponent, canActivate: [AuthGuard, OnlyAdminGuard] },
-  { path: 'helper/profile', component: HelperComponent, canActivate: [AuthGuard] }
+  { path: 'helper/profile', component: HelperComponent, canActivate: [AuthGuard] },
+  { path: '**',
+    redirectTo: '/sign-in',
+    pathMatch: 'full'
+  },
 ];
 
 @NgModule({
