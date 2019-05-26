ab7445e9c0ad7a916db4015b291bb8f5fcd7974a
Tony Warlamof <tonywarlamoff@gmail.com>
2019-05-26 06:57:46 +0300

    add cats

--- src/app/services/categories/categories.service.ts
+++ src/app/services/categories/categories.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Category } from 'src/app/types/categories.model';
+import { of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +10,19 @@ import { Category } from 'src/app/types/categories.model';
 export class CategoriesService {
   url = 'app/categories';
 
+  private cats?: Category[];
+
   constructor(
     private http: HttpClient,
   ) { }
 
   getCategories() {
-    return this.http.get<Category[]>(this.url);
+    if (this.cats) {
+      return of(this.cats);
+    }
+    return this.http.get<Category[]>(this.url)
+      .pipe(
+        tap(cats => { this.cats = cats; })
+      );
   }
 }
